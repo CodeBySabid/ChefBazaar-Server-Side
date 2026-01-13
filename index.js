110259795fde5db3c25f19413799b73f3eecd5ce
Sabid <sajjadsabid0@gmail.com>
2026-01-14 00:03:53 +0600

    Connected to the MongoDB

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT || 3000;
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middleware
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lqmwh22.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  const db = client.db('localchef_bazaar');
+  try {
+    await client.connect();
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send("Server is running")
+  res.send("Server is running")
 })
 
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
