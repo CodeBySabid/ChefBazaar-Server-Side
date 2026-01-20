eac2d8dfe19df24ebc4f43628a89898cd9fd28ec
Sabid <sajjadsabid0@gmail.com>
2026-01-21 00:29:52 +0600

    filtering user data for user profile from the users api

--- index.js
+++ index.js
@@ -6,28 +6,38 @@ const port = process.env.PORT || 3000;
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 // Middleware
+
 app.use(express.json());
 app.use(cors());
 
-const verifyAuthToken = async(req, res, next) => {
-  const token = req.headers.authorization;
-  if(!token){
-    return res.status(401).send({message: 'unauthorized access'});
+const verifyAuthToken = async (req, res, next) => {
+  const token = req.headers?.authorization;
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' });
   }
-  try{
+  try {
     const idToken = token.split(' ')[1];
     const decoded = await admin.auth().verifyIdToken(idToken);
-    if(req.decodedEmail = req.params.email) {
-      return res.status(403).send({ message: 'Forbidden access' })
-    }
-    req.decodedEmail = decoded.email;
+    req.decoded_Email = decoded.email;
+    // if(req.decoded_Email = req.params.email) {
+    //   return res.status(403).send({ message: 'Forbidden access' })
+    // }
     next();
   }
-  catch(err) {
-    return res.status(401).send({message: 'unauthorized access'});
+  catch (err) {
+    return res.status(401).send({ message: 'unauthorized access' });
   }
+  if (res.headersSent) return;
 }
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lqmwh22.mongodb.net/?appName=Cluster0`;
 
@@ -47,14 +57,23 @@ async function run() {
 
     app.post('/users', async (req, res) => {
       const user = req.body;
-      const existingUser = await usersCollection.findOne({email: user.email})
-      if(existingUser) {
-        return res.send({message: 'user already exists' })
+      const existingUser = await usersCollection.findOne({ email: user.email })
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
       }
       user.createdAt = new Date();
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
+
+    app.get('/users/:email', verifyAuthToken, async (req, res) => {
+      if (req.decoded_Email !== req.params.email) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+
+      const user = await usersCollection.findOne({ email: req.params.email });
+      res.send(user);
+    });
 
 
     // Send a ping to confirm a successful connection
