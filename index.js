58ea0fce2c4380a5b07ba9374c641349d2497d96
Sabid <sajjadsabid0@gmail.com>
2026-01-27 03:24:23 +0600

    Created a deleted favorite API for user favorite meal page.

--- index.js
+++ index.js
@@ -208,6 +208,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/favorite/:id', verifyAuthToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await favoritesCollection.deleteOne(query);
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
